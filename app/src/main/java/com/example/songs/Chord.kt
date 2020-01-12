0205d26e431ead87fd0f1265d3bed29ede4d4a57
titovtima <titov.tima.spb@yandex.ru>
2020-01-12 15:51:10 +0300

    chord types

--- app/src/main/java/com/example/songs/Chord.kt
+++ app/src/main/java/com/example/songs/Chord.kt
@@ -1,6 +1,23 @@
 package com.example.songs
 
 class Chord {
+    companion object {
+        val types = mapOf<Int,String>(
+            1 to "",
+            2 to "m",
+            3 to "7",
+            4 to "m7",
+            5 to "maj7",
+            7 to "sus4",
+            8 to "sus2",
+            9 to "2",
+            10 to "4",
+            11 to "6",
+            12 to "(+5)",
+            13 to "dim"
+        )
+    }
+
     val step: Int
     val type: String
     val bas_step: Int
